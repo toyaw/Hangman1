e3174d8f21065a77293f9cd4da672cb07f27418d
Toya Williams <toyaw7@gmail.com>
2019-06-13 22:14:37 -0400

    added the categories

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ $(document).ready(function() {
 });
 window.onload = function () {
 
-var alphabet + []
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y','z'];
+
+var theme;    //Summer Activites
+var secretword;  //choosen word
+var guess;        //guess
+var guesses = [ ]; // stored guesses
+var lives;      // lives
+var counter;    //count correct guess
+var blanks;     // spaces in the letters
+
+
+
+
 
 }
