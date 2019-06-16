dfb0391ac9adc62f3de557b3cae0efc281d920a4
Toya Williams <toyaw7@gmail.com>
2019-06-16 12:02:22 -0400

    added getElement

--- main.js
+++ main.js
@@ -13,3 +13,21 @@ Main.NumInWordBank = Words.Length;
 
 Main.Word = "test";
 Main.WordU = "";
+
+//functions
+
+Main.Pullword = function(){
+  Main.Word = Words.List[(Math.floor(Math.random()*Main.NumInWordBank))];
+
+}
+
+Main.SetUnderline = function(){
+  Main.Pullword();
+  for(i=0; i<Main.Word.length; i++){
+    Main.wordArray[i] = Main.Word.charAt(i);
+    Main.wordArray[i] = "-";
+  } 
+  Main.WordU = Main.wordArray.join("");
+  document.getElementById("WORD").innerHTML = Main.WordU;
+  document.getElementById("numLetters").innerHTML = Main.Word.length;
+}
