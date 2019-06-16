0bff93f4f8444c0638c103f27e96c833ae0bf482
Toya Williams <toyaw7@gmail.com>
2019-06-16 11:31:22 -0400

    adding wordbank

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 console.log("Hello from app.js")
 
-}
+
 $(document).ready(function () {
     alert("Good Luck!");
 });
+Main = {}
+Main.wordArray = [];
+Main.wordArray = [];
+
+Main.Lives = 4;
+Main.NumInWordBank = Words.Length;
+
+Main.Word = "test";
+Main.WordU = "";
