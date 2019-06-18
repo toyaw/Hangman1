b418b29717a487f314b1a34a830483aaf020983d
Toya Williams <toyaw7@gmail.com>
2019-06-17 21:52:33 -0400

    added questionbank alert

--- main.js
+++ main.js
@@ -12,4 +12,11 @@ let wordArray=new Array;
 //this will contain all the previous words guessed by player
 let previousGuesses=new Array;
 
-
+$.getJSON('wordbank.json', function(data) {
+  for(i=0; i<data.list.length; i++){
+    questionBank[i]=new Array;
+    questionBank[i][0] = data.list[i].word;
+    questionBank[i][1] = data.list[i].clue;
+  }
+  alert(questionBank);
+})//getJSON
