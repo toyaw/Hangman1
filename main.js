261ec9b4bf6f6e682dd08739833a52d22733a87a
Toya Williams <toyaw7@gmail.com>
2019-06-17 22:32:50 -0400

    removed json file and adding array to main.js

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 $(document).ready(function () {
     alert("Good Luck!");
 });
-let currentWord;
-let currentClue;
-//this trach the wrong answer entered
-let wrongAnswerCount;
-//array for question bank
-let questionBank=new Array;
-//word array for each word
-let wordArray=new Array;
-//this will contain all the previous words guessed by player
-let previousGuesses=new Array;
+//wordbank in an array
+let words = [
+  "cookout",
+  "water ballon",
+"kickball",
+"zoo",
+"drive in",
+"fishing",
+"vacation",
+"water sports",
+"sandcastle",
+"gardening",
+"kite",
+"beach",
+"park",
+"ice cream",
+"bubbles"
+];
 
-$.getJSON('wordbank.json', function(data) {
-  for(i=0; i<data.list.length; i++){
-    questionBank[i]=new Array;
-    questionBank[i][0] = data.list[i].word;
-    questionBank[i][1] = data.list[i].clue;
-  }
-  alert(questionBank);
-})//getJSON
+let word = words[Math.floor(math.random)]
